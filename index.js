bdec949cbbe48bd1ed571acc82224c54e21a96c5
Grace-aloo <graceaaloo@gmail.com>
2022-12-14 13:09:19 +0300

    website link

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ let container = document.getElementById('ticket')
 //function to update tickets
 function updatetickets(film){
    // console.log(film);
-    fetch(`http://localhost:3000/films/${film.id}`,{
+    fetch(`https://grace-aloo.github.io/db/db.json/${film.id}`,{
          method: 'PATCH',
          headers:{
             'Content-Type':'application/json'
